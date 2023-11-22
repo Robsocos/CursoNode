cbc08eed93d73674fb435c2ddd8d6926b2f84e5f
carvalhotaty <soarescarvalhotatiane@gmail.com>
2023-11-21 20:12:23 -0400

    pagina web na porta 3000

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ console.log('funcionou!')
 const express = require('express')
 const app = express()
 
+app.get('/',(req,res) => {
+    res.send("Olá WORLD")
+})
+
+app.get('/imagens',(req,res) =>{
+    res.send('Olá Imagens')
+} )
+
 app.listen(3000, () => {
 console.log('Servidor iniciado com sucesso')
 
 })
+
