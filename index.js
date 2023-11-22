1ca527dd4302b8488a9b99701530da6ddaa18712
carvalhotaty <soarescarvalhotatiane@gmail.com>
2023-11-21 21:24:08 -0400

    pag viacep

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const app = express()
 const path = require ('path')
 
 app.get('/',(req,res) => {
-    res.send("Olá WORLD")
+    res.json({
+        nome:"Robson",
+        sobrenome:"Costa"
+
+    })
 })
+
+app.get('/ws/69099173/json',(req,res) => {
+    res.json({
+        Cep:"69099173",
+        logradouro:"Tv mossoró",
+        complemento:"lado ímpar",
+        bairro:"Braga Mendes",
+        Localidade:"Manaus",
+        uf:"AM",
+        ibge:"3550308",
+        gia:"1004",
+        ddd:"92",
+        siafi:"7107",
+
+        })
+   })  
 
 app.get('/imagens',(req,res) =>{
     res.send('Olá Imagens')
@@ -14,6 +34,10 @@ app.get('/imagens',(req,res) =>{
 app.get('/sobre',(req, res) => {
  res.sendFile(path.resolve('sobre.html'))
 
+})
+
+app.get('/perguntasfrequentes', (req, res) => {
+    res.sendFile(path.resolve('perguntasfrequentes.html'))
 })
 
 app.listen(3000, () => {
